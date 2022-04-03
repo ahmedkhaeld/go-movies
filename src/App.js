c15ac71aa2eea856e32478b4cfaee09a33a4c5df
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-04-03 17:37:47 +0200

    refactor make movies by genres and call the api to fetch the genres

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from 'react';
 import {BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch} from 'react-router-dom';
-import Movies from './components/Movies'
+import Movies from './components/Movies';
 import Admin from './components/Admin'
 import Home from "./components/Home";
-import Categories from "./components/Categories"
-import OneMovie from "./components/OneMovie"
+import Genres from './components/Genres';
+import OneMovie from "./components/OneMovie";
 export default function App() {
   return (
       <Router>
@@ -28,7 +28,7 @@ export default function App() {
                     <Link to="/movies">Movies</Link>
                   </li>
                   <li className="list-group-item">
-                    <Link to="/by-category">Categories</Link>
+                    <Link to="/genres">Genres</Link>
                   </li>
                   <li className="list-group-item">
                     <Link to="/admin">Manage Catalogue</Link>
@@ -43,19 +43,10 @@ export default function App() {
                 <Route path="/movies">
                   <Movies />
                 </Route>
-                <Route exact path="/by-category">
-                  <CategoryPage />
+                <Route exact path="/genres">
+                  <Genres />
                 </Route>
-                <Route
-                    exact
-                    path="/by-category/drama"
-                    render={(props) => <Categories {...props} title={`Drama`} />}
-                />
-                <Route
-                    exact
-                    path="/by-category/comedy"
-                    render={(props) => <Categories {...props} title={`Comedy`} />}
-                />
+
                 <Route path="/admin">
                   <Admin />
                 </Route>
@@ -70,19 +61,3 @@ export default function App() {
   );
 }
 
-
-function CategoryPage() {
-
-  let { path, url } = useRouteMatch();
-
-  return (
-      <div>
-        <h2>Categories</h2>
-
-        <ul>
-          <li><Link to={`${path}/comedy`}>Comedy</Link> </li>
-          <li><Link to={`${url}/drama`}>Drama</Link> </li>
-        </ul>
-      </div>
-  );
-}
