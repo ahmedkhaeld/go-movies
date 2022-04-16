880cf282eac274ea1b78f176b44b3ccaf46eff37
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-04-16 17:42:36 +0200

    improve the form
    use the same form to edit an existing movie in the db
    create a components of the repeated html and use them as reusable dumb components in the form
    - inputs components
    - text area components
    -  select component

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from "react";
 import "./EditMovie.css";
+import Input from './form-components/Input';
+import TextArea from './form-components/TextArea';
+import Select from './form-components/Select';
 
 export default class EditMovie extends Component {
     constructor(props) {
@@ -14,6 +17,13 @@ export default class EditMovie extends Component {
                 rating: "",
                 description: "",
             },
+            mpaaOptions: [
+                {id: "G", value: "G"},
+                {id: "PG", value: "PG"},
+                {id: "PG13", value: "PG13"},
+                {id: "R", value: "R"},
+                {id: "NC17", value: "NC17"},
+            ],
             isLoaded: false,
             error: null,
         }
@@ -62,100 +72,59 @@ export default class EditMovie extends Component {
                         id="id"
                         value={movie.id}
                         onChange={this.handleChange}
-                    />                    <div className="mb-3">
-                        <label htmlFor="title" className="form-label">
-                            Title
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="title"
-                            name="title"
-                            value={movie.title}
-                            onChange={this.handleChange}
-                        />
-                    </div>
+                    />
+                    <Input
+                        title={"Title"}
+                        type={'text'}
+                        name={'title'}
+                        value={movie.title}
+                        handleChange={this.handleChange}
+                    />
 
-                    <div className="mb-3">
-                        <label htmlFor="release_date" className="form-label">
-                            Release date
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="release_date"
-                            name="release_date"
-                            value={movie.release_date}
-                            onChange={this.handleChange}
-                        />
-                    </div>
+                    <Input
+                        title={"Release Date"}
+                        type={'date'}
+                        name={'release_date'}
+                        value={movie.release_date}
+                        handleChange={this.handleChange}
+                    />
 
-                    <div className="mb-3">
-                        <label htmlFor="runtime" className="form-label">
-                            Runtime
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="runtime"
-                            name="runtime"
-                            value={movie.runtime}
-                            onChange={this.handleChange}
-                        />
-                    </div>
 
-                    <div className="mb-3">
-                        <label htmlFor="mpaa_rating" className="form-label">
-                            MPAA Rating
-                        </label>
-                        <select name="mpaa_rating" className="form-select" value={movie.mpaa_rating}>
-                            <option className="form-select">Choose...</option>
-                            <option className="form-select" value="G">
-                                G
-                            </option>
-                            <option className="form-select" value="PG">
-                                PG
-                            </option>
-                            <option className="form-select" value="PG13">
-                                PG13
-                            </option>
-                            <option className="form-select" value="R">
-                                R
-                            </option>
-                            <option className="form-select" value="NC17">
-                                NC17
-                            </option>
-                        </select>
-                    </div>
+                    <Input
+                        title={"Runtime"}
+                        type={'text'}
+                        name={'runtime'}
+                        value={movie.runtime}
+                        handleChange={this.handleChange}
+                    />
 
-                    <div className="mb-3">
-                        <label htmlFor="rating" className="form-label">
-                            Rating
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="rating"
-                            name="rating"
-                            value={movie.rating}
-                            onChange={this.handleChange}
-                        />
-                    </div>
 
-                    <div className="mb-3">
-                        <label htmlFor="description" className="form-label">
-                            Description
-                        </label>
-                        <textarea
-                            className="form-control"
-                            id="description"
-                            name="description"
-                            rows="3"
-                            onChange={this.handleChange}
-                            value={movie.value}
-                        />
-                    </div>
+                    <Input
+                        title={"Rating"}
+                        type={'text'}
+                        name={'rating'}
+                        value={movie.rating}
+                        handleChange={this.handleChange}
+                    />
 
+                    <Select
+                        title={'MPAA Rating'}
+                        name={'mpaa_rating'}
+                        options={this.state.mpaaOptions}
+                        value={movie.mpaa_rating}
+                        handleChange={this.handleChange}
+                        placeholder={'Choose...'}
+                    />
+
+
+
+                    <TextArea
+                        title={"Description"}
+                        name={"description"}
+                        value={movie.description}
+                         rows={"3"}
+                        handleChange={this.handleChange}
+                    />
                     <hr />
 
                     <button className="btn btn-primary">Save</button>
