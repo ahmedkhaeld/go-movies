858fe1123c1483e4cbffd2a5ee8f73ae1940131a
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-04-20 15:23:40 +0200

    add images to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-
+import Ticket from "./../images/movie_tickets.jpg";
 export default class Home extends Component{
     render() {
-        return(
-            <h2>Home</h2>
-        )
+        return (
+            <div className="text-center">
+                <h2>This is the home page</h2>
+                <hr />
+                <img src={Ticket} alt="movie ticket" />
+                <hr />
+                <div className="tickets"></div>
+            </div>
+        );
     }
 }
